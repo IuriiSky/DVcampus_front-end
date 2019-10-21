db6cd6d65ff17824131794d70ddd080a95c682cb
Iurii Stepanenko <uzik.fk11@gmail.com>
2019-10-21 21:13:43 +0300

    Install cssmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,22 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+        cssmin: {
+            options: {
+                sourceMap: true,
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'pub/css/responsive-style.min.css': ['assets/dist/css/responsive-style.css']
+                }
+            }
+        }
 
     });
-    
-    grunt.registerTask('default', []);
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['cssmin']);
 
 };
