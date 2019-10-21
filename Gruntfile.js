938e18cf77e7538b383503d38deb0586d1d555ae
Iurii Stepanenko <uzik.fk11@gmail.com>
2019-10-21 21:32:22 +0300

    Install postcss

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,15 +8,30 @@ module.exports = function(grunt) {
             },
             target: {
                 files: {
-                    'pub/css/responsive-style.min.css': ['assets/dist/css/responsive-style.css']
+                    'pub/css/responsive-style.min.css': ['assets/dist/css/responsive-style-processed.css']
                 }
             }
-        }
+        },
 
+        postcss: {
+            options: {
+                processors: [
+                    require('autoprefixer')(
+                        {overrideBrowsers: ['last 2 versions', 'ie 11']
+                        }), // add vendor prefixes
+                ]
+            },
+            dist: {
+                src: 'assets/dist/css/responsive-style.css',
+                dest: 'assets/dist/css/responsive-style-processed.css'
+            }
+        }
     });
 
+
+    grunt.loadNpmTasks('grunt-postcss');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.registerTask('default', ['cssmin']);
+    grunt.registerTask('default', ['postcss', 'cssmin']);
 
 };
